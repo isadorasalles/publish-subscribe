b11eb00baa7fa614abb97ca1502606219bef79e0
isadorasalles <isadorasalles@dcc.ufmg.br>
2021-01-14 13:38:45 -0300

    quase pronto

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:
 	g++ -Wall -c common.cpp
 	g++ -Wall client.cpp common.o -lpthread -o client
-	g++ -Wall server.cpp common.o -lpthread -o server
+	g++ -Wall server.cpp common.o -lpthread -o servidor
 
 clean:
 	rm common.o client server
--- client.cpp
+++ client.cpp
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <bits/stdc++.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -16,21 +17,44 @@ void usage(int argc, char **argv) {
 	exit(EXIT_FAILURE);
 }
 
-#define BUFSZ 1024
+#define BUFSZ 500
 
 struct connect_data{
 	int csock;
 };
 
 void *manda(void *data){
-	struct connect_data *cdata = (struct connect_data *)data;
-	char buf[BUFSZ];
-	memset(buf, 0, BUFSZ);
-	//printf("mensagem> ");
-	fgets(buf, BUFSZ-1, stdin);
-	size_t count = send(cdata->csock, buf, strlen(buf)+1, 0);
-	if (count != strlen(buf)+1) {
-		logexit("send");
+	while(1){
+		struct connect_data *cdata = (struct connect_data *)data;
+		char buf[BUFSZ];
+		memset(buf, 0, BUFSZ);
+		fgets(buf, BUFSZ-1, stdin);
+		std::string msg;// = "+dota\n+lol\n";
+		size_t count = send(cdata->csock, buf, BUFSZ+1 , 0);
+		// // std::cout << count << "\n";
+		// msg = "+teste\n #teste isso e uma mensagem ";
+		// count = send(cdata->csock, msg.c_str(), strlen(msg.c_str())+1 , 0);
+		// std::cout << count << "\n";
+		// msg = "particionada\n";
+		// count = send(cdata->csock, msg.c_str(), strlen(msg.c_str())+1 , 0);
+		if (count != strlen(buf)+1) {
+			logexit("send");
+		}
+	}
+	pthread_exit(EXIT_SUCCESS);
+}
+
+void *recebe(void *data){
+	while(1){
+		struct connect_data *cdata = (struct connect_data *)data;
+		char buf[BUFSZ];
+		memset(buf, 0, BUFSZ);
+		int total = 0;
+		size_t count = recv(cdata->csock, buf + total, BUFSZ - total, 0);
+		if (count == 0)
+		 	exit(0);
+		
+		std::cout << buf;
 	}
 	pthread_exit(EXIT_SUCCESS);
 }
@@ -59,26 +83,17 @@ int main(int argc, char **argv) {
 	addrtostr(addr, addrstr, BUFSZ);
 
 	printf("connected to %s\n", addrstr);
-	while(1){
 		
-		struct connect_data *cdata = (connect_data *) malloc(sizeof(struct connect_data));
-		cdata->csock = s;
-		pthread_t in;
-		pthread_create(&in, NULL, manda, cdata);
-		// pthread_t out;
-		// pthread_create(&out, NULL, recebe, cdata);
+	struct connect_data *cdata = (connect_data *) malloc(sizeof(struct connect_data));
+	cdata->csock = s;
+	pthread_t in;
+	pthread_t out;
+	pthread_create(&in, NULL, manda, cdata);
+	pthread_create(&out, NULL, recebe, cdata);
 
-
-		char buf[BUFSZ];
-		memset(buf, 0, BUFSZ);
-		unsigned total = 0;
-		size_t count = recv(cdata->csock, buf + total, BUFSZ - total, 0);
-		total += count;
-		
-
-		//printf("received %u bytes\n", total);
-		puts(buf);
-	}
+	pthread_join(in, NULL);
+	pthread_join(out, NULL);
+	
 	close(s);
 
 	exit(EXIT_SUCCESS);
